d3394aa30134b590cb31ffc565322740ca0d7ff3
szdenes <57998732+szdenes@users.noreply.github.com>
2020-06-09 17:39:08 +0200

    push#25

--- js/submit.js
+++ js/submit.js
@@ -1,41 +1,54 @@
-$("#button-cp").click(function() {
+$("#button").click(function() {
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function() {
+        'use strict';
+        window.addEventListener('load', function() {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function(form) {
+                form.addEventListener('submit', function(event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
 
-    // remove blue button, transition to grey loading circle - change 1500 to loadtime of when form is completed
-    $("#button-cp").addClass("grey-button");
-    $("#circle").addClass("grey-outline");
-
-    setTimeout(function() {
-        $(".grey-button").addClass("disappear");
-        $("#finish").removeClass("disappear");
-        $("#form").addClass("disappear");
+                }, false);
+            });
+        }, false);
+    })();
 
 
-        // once grey loading circle is complete, remove submission form, make confirmation text appear
+    if ($("#gsheetForm").hasClass("was-validated")) {
 
-        $(".column").addClass("disappear");
-        $("#confirm").animate({
-            width: 'toggle'
-        });
-
-
-        $("#circle").removeClass("grey-outline");
-
+        // remove blue button, transition to grey loading circle - change 1500 to loadtime of when form is completed
+        $("#button").addClass("grey-button");
+        $("#circle").addClass("grey-outline");
         setTimeout(function() {
-            $(".column").removeClass("disappear").fadeIn('slow');
-        }, 500);
+            $(".grey-button").addClass("disappear");
+            $("#finish").removeClass("disappear");
+            // $("#form").addClass("disappear");
 
-    }, 1500);
+
+            // once grey loading circle is complete, remove submission form, make confirmation text appear
+
+            $(".column").addClass("disappear");
+            // $("#confirm").animate({
+            //     width: 'toggle'
+            // });
+
+            $("#circle").removeClass("grey-outline");
+
+            setTimeout(function() {
+                $(".column").removeClass("disappear").fadeIn('slow');
+            }, 500)
+
+        }, 1500);
 
 
+
+
+    }
 
 });
-
-// GENERAL STEPS
-
-// onclick of blue button
-
-// 1 - shrink width of blue button
-// 2 - add class(grey-button)
-
-// on completion
-// 1 - add class(disappear) to form
