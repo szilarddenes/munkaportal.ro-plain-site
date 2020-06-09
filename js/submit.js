9af57561428a6c3966dd2f22f1971c8110e3dd68
szdenes <57998732+szdenes@users.noreply.github.com>
2020-06-09 10:10:28 +0200

    push#24

--- js/submit.js
+++ js/submit.js
@@ -1,60 +1,10 @@
-var basicTimeline = anime.timeline({
-  autoplay: false,
-});
+var wrapper = $("#button-wrapper");
 
-var pathEls = $('.checkSubmit');
-for (var i = 0; i < pathEls.length; i++) {
-  var pathEl = pathEls[i];
-  var offset = anime.setDashoffset(pathEl);
-  pathEl.setAttribute('stroke-dashoffset', offset);
-}
-
-basicTimeline
-  .add({
-    targets: '.textSubmit',
-    duration: 1,
-    opacity: '0',
-  })
-  .add({
-    targets: '.buttonSubmit',
-    duration: 1300,
-    height: 10,
-    width: 300,
-    backgroundColor: '#2B2D2F',
-    border: '0',
-    borderRadius: 100,
-  })
-  .add({
-    targets: '.progress-barSubmit',
-    duration: 2000,
-    width: 300,
-    easing: 'linear',
-  })
-  .add({
-    targets: '.buttonSubmit',
-    width: 0,
-    duration: 1,
-  })
-  .add({
-    targets: '.progress-barSubmit',
-    width: 80,
-    height: 80,
-    delay: 500,
-    duration: 750,
-    borderRadius: 80,
-    backgroundColor: '#71DFBE',
-  })
-  .add({
-    targets: pathEl,
-    strokeDashoffset: [offset, 0],
-    duration: 200,
-    easing: 'easeInOutSine',
-  });
-
-$('.buttonSubmit').click(function () {
-  basicTimeline.play();
-});
-
-$('.textSubmit').click(function () {
-  basicTimeline.play();
+$(".submit").click(function() {
+    if (wrapper.not(".checked")) {
+        wrapper.addClass("checked");
+        setTimeout(function() {
+            wrapper.removeClass("checked");
+        }, 8000);
+    }
 });
