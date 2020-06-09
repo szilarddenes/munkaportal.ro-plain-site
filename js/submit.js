368cbef8b127178f67f3a29b535f0a38fa4e9628
szdenes <57998732+szdenes@users.noreply.github.com>
2020-06-09 10:44:35 +0200

    push#25

--- js/submit.js
+++ js/submit.js
@@ -1,10 +1,41 @@
-var wrapper = $("#button-wrapper");
+$("#button-cp").click(function() {
 
-$(".submit").click(function() {
-    if (wrapper.not(".checked")) {
-        wrapper.addClass("checked");
+    // remove blue button, transition to grey loading circle - change 1500 to loadtime of when form is completed
+    $("#button-cp").addClass("grey-button");
+    $("#circle").addClass("grey-outline");
+
+    setTimeout(function() {
+        $(".grey-button").addClass("disappear");
+        $("#finish").removeClass("disappear");
+        $("#form").addClass("disappear");
+
+
+        // once grey loading circle is complete, remove submission form, make confirmation text appear
+
+        $(".column").addClass("disappear");
+        $("#confirm").animate({
+            width: 'toggle'
+        });
+
+
+        $("#circle").removeClass("grey-outline");
+
         setTimeout(function() {
-            wrapper.removeClass("checked");
-        }, 8000);
-    }
+            $(".column").removeClass("disappear").fadeIn('slow');
+        }, 500);
+
+    }, 1500);
+
+
+
 });
+
+// GENERAL STEPS
+
+// onclick of blue button
+
+// 1 - shrink width of blue button
+// 2 - add class(grey-button)
+
+// on completion
+// 1 - add class(disappear) to form
