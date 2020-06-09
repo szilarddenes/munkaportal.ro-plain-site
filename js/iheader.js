f00c89f241ebc522ba6c47f439a9717706b50c5b
szdenes <57998732+szdenes@users.noreply.github.com>
2020-06-09 08:14:40 +0200

    push#23

--- js/iheader.js
+++ js/iheader.js
@@ -1,58 +1,58 @@
-(function () {
-  var doc = document.documentElement;
-  var w = window;
+(function() {
+    var doc = document.documentElement;
+    var w = window;
 
-  var prevScroll = w.scrollY || doc.scrollTop;
-  var curScroll;
-  var direction = 0;
-  var prevDirection = 0;
-  var header = document.getElementById('site-header');
-  var pageYOffset = 0;
-  header.classList.add('transpp');
+    var prevScroll = w.scrollY || doc.scrollTop;
+    var curScroll;
+    var direction = 0;
+    var prevDirection = 0;
+    var header = document.getElementById('site-header');
+    var pageYOffset = 0;
+    header.classList.add('transpp');
 
-  var checkScroll = function () {
-    /*
-     ** Find the direction of scroll
-     ** 0 - initial, 1 - up, 2 - down
-     */
-    //set transparent by default
+    var checkScroll = function() {
+        /*
+         ** Find the direction of scroll
+         ** 0 - initial, 1 - up, 2 - down
+         */
+        //set transparent by default
 
-    curScroll = w.scrollY || doc.scrollTop || pageYOffset;
-    if (curScroll > prevScroll) {
-      //scrolled up
-      direction = 2;
-    } else if (curScroll < prevScroll) {
-      //scrolled down
-      direction = 1;
-    }
+        curScroll = w.scrollY || doc.scrollTop || pageYOffset;
+        if (curScroll > prevScroll) {
+            //scrolled up
+            direction = 2;
+        } else if (curScroll < prevScroll) {
+            //scrolled down
+            direction = 1;
+        }
 
-    if (direction !== prevDirection) {
-      toggleHeader(direction, curScroll);
-    }
-    if (window.pageYOffset <= 5) {
-      header.classList.add('transpp');
-      // prevDirection = direction;
-    } else if (window.pageYOffset >= 5) {
-      header.classList.remove('transpp');
-      // prevDirection = direction;
-    }
+        if (direction !== prevDirection) {
+            toggleHeader(direction, curScroll);
+        }
+        if (window.pageYOffset <= 5) {
+            header.classList.add('transpp');
+            // prevDirection = direction;
+        } else if (window.pageYOffset >= 5) {
+            header.classList.remove('transpp');
+            // prevDirection = direction;
+        }
 
-    prevScroll = curScroll;
-  };
+        prevScroll = curScroll;
+    };
 
-  var toggleHeader = function (direction, curScroll) {
-    if (direction === 2 && curScroll > 10) {
-      //replace 52 with the height of your header in px
+    var toggleHeader = function(direction, curScroll) {
+        if (direction === 2 && curScroll > 20) {
+            //replace 52 with the height of your header in px
 
-      header.classList.add('hide');
-      prevDirection = direction;
-    } else if (direction === 1) {
-      header.classList.remove('hide');
-      prevDirection = direction;
-    }
-  };
+            header.classList.add('hide');
+            prevDirection = direction;
+        } else if (direction === 1) {
+            header.classList.remove('hide');
+            prevDirection = direction;
+        }
+    };
 
-  window.addEventListener('scroll', checkScroll);
+    window.addEventListener('scroll', checkScroll);
 })();
 
 // What it does
