3a05a391eed9fc8ee0c7278d8723d672d96bd9f3
szdenes <57998732+szdenes@users.noreply.github.com>
2020-06-11 23:55:06 +0200

    push#38

--- .history/js/submit_20200611235223.js
+++ .history/js/submit_20200611235223.js
@@ -38,7 +38,7 @@ form.addEventListener('submit', (e) => {
     })
       .then((response) => console.log('Success!', response))
       .catch((error) => console.error('Error!', error.message));
-      submitButtonA();
+    UIProgressButton(el, option);
   }
 });
 
@@ -211,46 +211,7 @@ function isEmail(email) {
   );
 }
 
-function submitButtonA() {
-    var clicked = false;
-    var submit = document.querySelector('.submit');
-    submit.addEventListener("click", function(){
-      // Make sure user cannot click button again until it has been reset
-      if( !clicked ){
-        clicked = true;
-        submit.classList.remove("return");
-        submit.blur();
-        document.querySelector('.loading-dock').classList.add('loaded');
-        document.getElementById('load').style.display= 'initial';
-        document.getElementById('load-b').style.display= 'initial';
-        setTimeout(function(){
-          document.getElementById('load').style.opacity = 1;
-        }, 750);
-        setTimeout(function(){
-          document.getElementById('load-b').style.opacity = 1;
-        }, 900);
-        setTimeout(function(){
-          document.querySelector('.loading-dock').classList.remove('loaded');
-          document.getElementById('load').style.display = 'none';
-          document.getElementById('load-b').style.display = 'none';
-          document.getElementById('load').style.opacity = 0;
-          document.getElementById('load-b').style.opacity = 0;
-          let submit = document.querySelector('.submit');
-          submit.classList.add("popout");
-          submit.innerHTML = "";
-          setTimeout(function(){
-          document.getElementById('check').style.display = "block";
-          }, 300);
-        }, 3600);
-    
-        //reset all
-        setTimeout(function(){
-          submit.classList.remove("popout");
-          submit.classList.add("return");
-          submit.innerHTML = "Submit";
-          document.getElementById('check').style.display = "none";
-          clicked = false;
-        }, 5300);
-      }
-    })
+function submitButton(el, options) {
+  document.getElementById('buttonSubmit').style.color = 'red';
+  // return true;
 }
