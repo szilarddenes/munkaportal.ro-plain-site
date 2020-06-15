8589c60d9c53cf906bb91759e94529fbd6181148
szdenes <57998732+szdenes@users.noreply.github.com>
2020-06-15 16:42:48 +0200

    push#50

--- js/carouselnrm.js
+++ js/carouselnrm.js
@@ -2,21 +2,25 @@
  * src: https://snook.ca/archives/javascript/normalize-bootstrap-carousel-heights?fbclid=IwAR31WXAwZHInpbVKG9nx3O2LVit1FiO8jlhh2xdXflq63ueIH_cU58RZ_ZE */
 
 function normalizeSlideHeights() {
-  $('.carousel').each(function () {
-    var items = $('.carousel-item', this);
-    // reset the height
-    items.css('min-height', 0);
-    // set the height
-    var maxHeight = Math.max.apply(
-      null,
-      items
-        .map(function () {
-          return $(this).outerHeight();
-        })
-        .get()
-    );
-    items.css('min-height', maxHeight + 'px');
-  });
+    $('.carousel').each(function() {
+        var items = $('.carousel-item', this);
+        // reset the height
+        items.css('min-height', 0);
+        // set the height
+        var maxHeight = Math.max.apply(
+            null,
+            items
+            .map(function() {
+                return $(this).outerHeight();
+            })
+            .get()
+        );
+        items.css('min-height', maxHeight + 'px');
+    });
 }
 
 $(window).on('load resize orientationchange', normalizeSlideHeights);
+
+$('.navbar-nav>li>a').on('click', function() {
+    $('.navbar-collapse').collapse('hide');
+});
