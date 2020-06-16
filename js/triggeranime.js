624cbffb87f01239eabf78c0f4bb6af2562d5401
szdenes <57998732+szdenes@users.noreply.github.com>
2020-06-16 13:07:42 +0200

    push#55 -- triggeranime.js live

--- js/triggeranime.js
+++ js/triggeranime.js
@@ -1,35 +1,102 @@
-var myElement = document.getElementById('count1');
-var bounding = myElement.getBoundingClientRect();
-var myElementHeight = myElement.offsetHeight;
-var myElementWidth = myElement.offsetWidth;
-let inviewport = [];
-console.log(myElement.offsetWidth);
-console.log(myElement.offsetHeight);
-console.log(myElement.getBoundingClientRect);
-console.log(window.innerHeight);
+/*!
+ * Determine if an element is in the viewport
+ * (c) 2017 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {Node}    elem The element
+ * @return {Boolean}      Returns true if element is in the viewport
+ */
+var isInViewport = function(elem) {
+    var distance = elem.getBoundingClientRect();
+    return (
+        distance.top >= 0 &&
+        distance.left >= 0 &&
+        distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
 
-function elementInViewport() {
-  var bounding = myElement.getBoundingClientRect();
+//heartbeat start
+var heart = document.getElementById('svgFacebookwhiteHeart');
+window.addEventListener('scroll', function(event) {
+    if (isInViewport(svgFacebookwhiteHeart)) {
+        heart.classList.add("beatheart");
+        // console.log(`in viewport`);
+    } else {
+        // console.log(`NOT in viewport`);
+    }
+});
 
-  if (
-    bounding.top >= -myElementHeight &&
-    bounding.left >= -myElementWidth &&
-    bounding.right <=
-      (window.innerWidth || document.documentElement.clientWidth) +
-        myElementWidth &&
-    bounding.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) +
-        myElementHeight
-  ) {
-       inviewport = 1;
-    console.log('Element is in the viewport!');
-    document.getElementById("count1").style.color = "blue";
-  } else {
-    console.log('Element is NOT in the viewport!');
-    inviewport = 2;
-  }
- 
+//heartbeat end
+
+//counter start
+let start // set on the first step to the timestamp provided
+const el1 = document.getElementById('count1') // get the element
+const el2 = document.getElementById('count2') // get the element
+const el3 = document.getElementById('count3') // get the element
+const final1 = parseInt(el1.textContent, 10) // parse out the final number
+const final2 = parseInt(el2.textContent, 10) // parse out the final number
+const final3 = parseInt(el3.textContent, 10) // parse out the final number
+const duration = 3500 // duration in ms
+var countedUp = false;
+window.addEventListener('scroll', function(event) {
+    if (isInViewport(count1) && (countedUp == false)) {
+
+        el1.classList.add("animate__headShake");
+        el2.classList.add("animate__headShake");
+        el3.classList.add("animate__headShake");
+        countUp();
+
+        console.log(`count1 in viewport`);
+    } else {
+        console.log(`count1 NOT in viewport`);
+    }
+});
+
+function countUp() {
+    const step = ts => {
+        if (!start) {
+            start = ts
+        }
+        // get the time passed as a fraction of total duration
+        let progress = (ts - start) / duration
+
+        el1.textContent = Math.floor(progress * final1) // set the text
+        el2.textContent = Math.floor(progress * final2) // set the text
+        el3.textContent = Math.floor(progress * final3) // set the text
+        if (progress < 1) {
+            // if we're not 100% complete, request another animation frame
+            requestAnimationFrame(step)
+        }
+        countedUp = true;
+    }
+
+    // start the animation
+    requestAnimationFrame(step)
+
 }
-console.log('inviewport',inviewport);
 
-//id="svgFacebookwhiteHeart" class="st1 beatheart"
+//counter end
+
+// social start
+var soc1 = document.getElementById('socIG');
+var soc2 = document.getElementById('socFB');
+var soc3 = document.getElementById('socIN');
+window.addEventListener('scroll', function(event) {
+    if (isInViewport(soc1)) {
+        setTimeout(flash(soc1, 2000));
+        setTimeout(flash(soc2, 2000));
+        setTimeout(flash(soc3, 2000));
+
+        // console.log(`in viewport`);
+    } else {
+        // console.log(`NOT in viewport`);
+    }
+});
+
+function flash(input) {
+    input.classList.add("model-3d-0");
+    input.classList.add("back");
+    input.classList.add("model-3d-0");
+    input.classList.remove("back");
+}
+
+// social end
